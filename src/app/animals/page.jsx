8dd6665c85ffd91aa9b4ee4d8f94f1022db5fa12
azim024471-netizen <azim024471@gmail.com>
2026-05-04 14:17:40 +0600

    completed the proxy.js and added the 404 page

--- src/app/animals/page.jsx
+++ src/app/animals/page.jsx
@@ -6,8 +6,8 @@ import FeaturedCard from '../components/featured/FeaturedCard';
 const Animals = async () => {
 
 
-    // const res = await fetch('https://hattify-assignment-8.vercel.app/data.json');
-    const res = await fetch('http://localhost:3000/data.json');
+    const res = await fetch('https://hattify-assignment-8.vercel.app/data.json');
+    // const res = await fetch('http://localhost:3000/data.json');
 
 
     const animals = await res.json();
