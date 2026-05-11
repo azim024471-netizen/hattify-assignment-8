cad91d88620ba0f4f0a42b27ac2542180d57431f
azim024471-netizen <azim024471@gmail.com>
2026-05-11 12:20:41 +0600

    fixed the  nav var more responsive for mobile device

--- src/app/animals/page.jsx
+++ src/app/animals/page.jsx
@@ -4,8 +4,8 @@
 import AllAnimals from '../components/animals/AnimalCard';
 
 const Animals = async () => {    
-    const res = await fetch('http://localhost:3000/data.json');
-    // const res = await fetch('https://hattify-assignment-8.vercel.app/data.json');
+    // const res = await fetch('http://localhost:3000/data.json');
+    const res = await fetch('https://hattify-assignment-8.vercel.app/data.json');
      const animals= await res.json(); 
      
 
